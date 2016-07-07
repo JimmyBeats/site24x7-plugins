bf0c9050ca3b52ba13c51a82b75fffe741aa1fcb
James Beattie <jbeattie@agentbox.com.au>
2016-07-07 11:37:03 +1000

    Add processlist and userlist plugins to repo

---
+++ README.md
@@ -0,0 +1,7 @@
+Additional plugins for Site24x7 monitoring https://www.site24x7.com/
+
+Processlist - Logs a list of the top 20 processes grouped by the process name
+ and number of instances of that process running
+
+Userlist - Logs a list of users and the number of processes running under that
+ user (useful for shared hosting type setups)
---
+++ userlist/userlist.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import json
+import platform
+import subprocess
+
+#if any impacting changes to this plugin kindly increment the plugin version here.
+PLUGIN_VERSION = "1"
+
+#Setting this to true will alert you when there is a communication problem while posting plugin data to server
+HEARTBEAT="true"
+
+TOPCOMMAND='ps -eo user=|sort|uniq -c'
+
+def metricCollector():
+    data = {}
+    data['plugin_version'] = PLUGIN_VERSION
+    data['heartbeat_required']=HEARTBEAT
+
+    try:
+        proc = subprocess.Popen(TOPCOMMAND,stdout=subprocess.PIPE,close_fds=True,shell=True)
+        top_output = proc.communicate()[0]
+        for line in top_output.split('\n'):
+            if not line:
+                continue
+            line_raw = line.split(' ')
+            data[line_raw[-1]] = line_raw[-2]
+    except Exception as exception:
+        data['status']=0
+        data['msg']='error while executing top command'
+
+    return data
+
+
+if __name__ == '__main__':
+
+    print json.dumps(metricCollector(), indent=4, sort_keys=True)
