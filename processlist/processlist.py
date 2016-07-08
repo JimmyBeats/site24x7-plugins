4a62b0277e078a511001f7e5a94e9d58e7e4ef43
James Beattie <jbeattie@agentbox.com.au>
2016-07-08 10:39:21 +1000

    Tweaks to process list

--- processlist/processlist.py
+++ processlist/processlist.py
@@ -5,7 +5,7 @@ import platform
 import subprocess
 
 #if any impacting changes to this plugin kindly increment the plugin version here.
-PLUGIN_VERSION = "3"
+PLUGIN_VERSION = "4"
 
 #Setting this to true will alert you when there is a communication problem while posting plugin data to server
 HEARTBEAT="true"
@@ -21,12 +21,17 @@ def metricCollector():
         proc = subprocess.Popen(TOPCOMMAND,stdout=subprocess.PIPE,close_fds=True,shell=True)
         top_output = proc.communicate()[0]
         for line in top_output.split('\n'):
-            if not line:
-                continue
-            if '<defunct>' in line:
-                continue
-            line_raw = line.split(' ')
-            data[line_raw[-1]] = line_raw[-2]
+           if not line:
+               continue
+           if '<defunct>' in line:
+               continue
+           if '/' in line:
+               continue
+           line_raw = line.split(' ')
+           value = line_raw[-2]
+           if '-' in value:
+               continue
+           data[line_raw[-1]] = line_raw[-2]
     except Exception as exception:
         data['status']=0
         data['msg']='error while executing top command'
