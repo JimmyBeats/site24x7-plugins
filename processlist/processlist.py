d7f17a1a11a41e8eb1b1c4fce76d1849bace6eeb
James Beattie <jbeattie@agentbox.com.au>
2016-07-08 11:09:22 +1000

    More refinement as to which processes we are going to monitor

--- processlist/processlist.py
+++ processlist/processlist.py
@@ -5,7 +5,7 @@ import platform
 import subprocess
 
 #if any impacting changes to this plugin kindly increment the plugin version here.
-PLUGIN_VERSION = "4"
+PLUGIN_VERSION = "5"
 
 #Setting this to true will alert you when there is a communication problem while posting plugin data to server
 HEARTBEAT="true"
@@ -23,15 +23,35 @@ def metricCollector():
         for line in top_output.split('\n'):
            if not line:
                continue
+
+           # Not interested in defunct processes
            if '<defunct>' in line:
                continue
+
+           # Not interested in processes with slashes
            if '/' in line:
                continue
+
+           # split the line so we have key => val
            line_raw = line.split(' ')
+
+           # get the value
            value = line_raw[-2]
+
+           # if hyphen, we not interested
            if '-' in value:
                continue
-           data[line_raw[-1]] = line_raw[-2]
+
+           # if the value is only 1, then also not interested (only tracking processes that have multiple)
+           if (value > 1):
+               data[line_raw[-1]] = value
+
+           # now make sure that any process we are interested have been included (even if none currently running)
+           required_keys = ['php-cli', 'httpd', 'php', 'run-php', 'run-php-lock', 'redis-server', 'exim']
+           for key in required_keys:
+               if key not in data:
+                   data[key] = 0
+
     except Exception as exception:
         data['status']=0
         data['msg']='error while executing top command'
